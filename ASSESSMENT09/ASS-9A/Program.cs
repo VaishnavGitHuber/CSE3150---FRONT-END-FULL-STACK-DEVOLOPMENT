f2927f62f26f5b4196581467883fbad61d049b36
Vaishnav Krishna P <124416940+VaishnavGitHuber@users.noreply.github.com>
2025-04-21 14:42:11 +0530

    ASSESSMENT 9

---
+++ ASSESSMENT09/ASS-9A/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyConsoleApp
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+    }
+
+    public class StudentDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=localhost,1433;Database=STUDENT_EF;User Id=SA;Password=MyPassword#321;TrustServerCertificate=True");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var Context = new StudentDbContext())
+            {
+                Student NewStudent = new Student()
+                {
+                    StudentName = "Dhanush SN"
+                };
+                Context.Students.Add(NewStudent);
+                Context.SaveChanges();
+
+                foreach (var item in Context.Students) {
+                    Console.WriteLine(item.StudentId + " " + item.StudentName);
+                }
+            
+            }
+            
+
+        }
+    }
+}
---
+++ ASSESSMENT09/ASS-9B/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyConsoleApp
+{
+    public class Doctor
+    {
+        public int DoctorId { get; set; }
+        public string DoctorName { get; set; }
+        public string Specialisation { get; set; }
+        public int ConsultationFees{ get; set; }
+    }
+
+    public class DoctorDbContext : DbContext
+    {
+        public DbSet<Doctor> Doctors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=localhost,1433;Database=DOCTOR;User Id=SA;Password=MyPassword#321;TrustServerCertificate=True");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using(var context = new DoctorDbContext()){
+                Doctor NewDoctor = new Doctor()
+                {
+                    DoctorName = "Dhanush Sn",
+                    Specialisation = "ENT",
+                    ConsultationFees=1200
+                };
+            // Adding the new doctor info
+            context.Doctors.Add(NewDoctor);
+            context.SaveChanges();
+
+                // Display the details 
+                foreach (var record in context.Doctors)
+                {
+                    Console.Write(record.DoctorId + " ");
+                    Console.Write(record.DoctorName + " ");
+                    Console.Write(record.Specialisation + " ");
+                    Console.Write(record.ConsultationFees + " ");
+                    Console.WriteLine();
+                }
+            
+        }
+}
+    }
+}
